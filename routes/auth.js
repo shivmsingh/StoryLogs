c7bf8448a04ec8b63e08ea2f1e5970f93538b77c
Shivam Singh <shivmsingh02@gmail.com>
2023-01-15 20:03:00 +0530

    Initial Deploy

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const passport = require('passport')
-const router = express.Router()
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
 
 // @desc    Auth with Google
 // @route   GET /auth/google
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
 router.get(
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    res.redirect('/dashboard')
+    res.redirect("/dashboard");
   }
-)
+);
 
 // @desc    Logout user
 // @route   /auth/logout
-router.get('/logout', (req, res, next) => {
-  req.logout((error) => {
-      if (error) {return next(error)}
-      res.redirect('/')
-  })
-})
+router.get("/logout", function (req, res) {
+  req.session.destroy(function (err) {
+    res.redirect("/"); //Inside a callback… bulletproof!
+  });
+});
 
-module.exports = router
+module.exports = router;
